31d0047ec83ccdf597a7f08769dfd139f7e6b076
AxeZzz <110597180+AxeZzz@users.noreply.github.com>
2022-11-11 20:22:16 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'AxeZzz'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6285643117836','6285643117836','6285643117836']
+global.premium = ['6285643117836']
+global.packname = 'AxeZzz'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
